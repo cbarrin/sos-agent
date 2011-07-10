941b468fcc4139235f539ad7b77e194b74e90870
arosen <arosen@arosen-home.lan>
2011-07-09 20:54:11 -0400

    Added testsuite

---
+++ testsuite/server.c
@@ -0,0 +1,155 @@
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+
+
+#define PORT "3490"  // the port users will be connecting to
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+#define MAXDATASIZE 100
+#define MAX_RUNS 100000
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+void setnonblocking(int sock)
+{
+   int opts;
+
+   opts = fcntl(sock,F_GETFL);
+   if (opts < 0)
+   {
+      perror("fcntl(F_GETFL)");
+      exit(EXIT_FAILURE);
+   }
+   opts = (opts | O_NONBLOCK);
+   if (fcntl(sock,F_SETFL,opts) < 0)
+   {
+      perror("fcntl(F_SETFL)");
+      exit(EXIT_FAILURE);
+   }
+   return;
+}
+
+
+
+
+int main(void)
+{
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	int rv;
+	char buf[MAXDATASIZE]; 
+	int ret; 
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+	sin_size = sizeof their_addr;
+	new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+	if (new_fd == -1) {
+		perror("accept");
+		exit(1); 
+	}
+
+		close(sockfd); // child doesn't need the listener
+		setnonblocking(new_fd); 
+		
+		while(1) { 
+			if ((recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) {
+
+				}
+			if ((ret = send(new_fd, "Hello, world!", 99, 0)) == -1) { 
+					if (errno == ECONNRESET) break ; 
+
+					perror("send()"); 
+		
+			} 
+			if(!ret) {
+				printf("closing connection\n"); 
+				break; 
+			} 
+		}
+		close(new_fd);
+		exit(0);
+
+	return 0;
+}
+
