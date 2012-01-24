b6b496557370327aac6c613d02795c4078b02166
Aaron Rosen <arosen@clemson.edu>
2012-01-23 23:31:59 -0500

    changed value

--- iperf.py
+++ iperf.py
@@ -5,9 +5,9 @@ import subprocess
 class Iperf:
 	def __init__(self):
 		self.runs = 100
-		self.host = "awesome"
+		self.host = "10.0.0.13"
 		self.time = "10"
-		self.port = "5001"
+		self.port = "5004"
 	def start(self):
 		for i in range(0, self.runs):	
 			subprocess.call(["iperf", "-c", self.host, "-p", self.port, "-t", self.time])
