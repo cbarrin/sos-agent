e2c7b7b2e827514225172aaeaba79a33b638b657
Geddings Barrineau <cbarrin@g.clemson.edu>
2016-01-25 12:39:09 -0500

    Removed sigint handler in main and changed broadcast addresses for cloudlab

--- agent.c
+++ agent.c
@@ -52,7 +52,7 @@ int main(int argc, char **argv) {
     agent_t agent;
     memset(&agent, 0, sizeof(agent_t));
     
-    signal(SIGINT, sigint_handler);
+    //signal(SIGINT, sigint_handler);
 
     get_arguments(&agent.options, argc, argv);
     init_agent(&agent);
